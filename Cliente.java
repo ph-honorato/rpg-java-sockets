690f2e4ed77a3a195eb26add641ea66074e2a5fb
pedro <ph.honorato28@gmail.com>
2022-09-15 10:30:50 -0300

    Atualizando o cliente

--- Cliente.java
+++ Cliente.java
@@ -4,6 +4,60 @@ import java.io.PrintStream;
 import java.net.*;
 import java.util.Scanner;
 
+class personagem {
+
+    public String classe;
+    public int maxVida;
+    public int atualVida;
+    public int dano;
+    public String acao;
+
+    public personagem(String classe, int maxVida, int atualVida, int dano, String acao){
+        this.classe = classe;
+        this.maxVida = maxVida;
+        this.atualVida = atualVida;
+        this.dano = dano;
+        this.acao = acao;
+    }
+
+}
+
+
+class historia {
+
+    public void escolherPersonagem(){
+        String [] linhas = {
+            "",
+            "",
+            "",
+            "",
+            "Existem quatro opções de classe:",
+            "",
+            "Arqueiro (1)\tBárbaro (2)\tCavaleiro (3)\tMago (4)",
+            "",
+            "Vida: 20\tVida: 25\tVida: 30\tVida: 15",
+            "Ataque: 1xD8\tAtaque: 1xD10\tAtaque: 1xD6\tAtaque: 1xD6\t",
+            "Ação: Mirar\tAção: Fúria\tAção: Defender\tAção: Curar",
+            "",
+            "",
+            "",
+        };
+
+        for (String l: linhas){System.out.println(l);}
+    }
+
+    public void personagemEscolhido(String p){
+        String [] linhas = {
+            "",
+            "Você se conectou utilizando a classe: " + p,
+            "",
+        };
+
+        for (String l: linhas){System.out.println(l);}
+    }
+}
+
+
 class Recebedor implements Runnable {
     
     private InputStream servidor;
@@ -30,7 +84,15 @@ public class Cliente {
     private static String host = "127.0.0.1";
     private static int porta = 12345;
 
+    public static Scanner teclado = new Scanner(System.in);
+    public static historia h = new historia();
+    public static personagem p;
+
     public static void main(String[] args) throws UnknownHostException, IOException {    
+
+        h.escolherPersonagem();
+        p = escolherPersonagem();
+
         // dispara cliente
         executa();
     }
@@ -38,21 +100,48 @@ public class Cliente {
     public static void executa() throws UnknownHostException, IOException {
         
         Socket cliente = new Socket(host, porta);
-        System.out.println("O cliente se conectou ao servidor!");
+        h.personagemEscolhido(p.classe);
 
         // thread para receber mensagens do servidor
         Recebedor r = new Recebedor(cliente.getInputStream());
         new Thread(r).start();
         
         // lê msgs do teclado e manda pro servidor
-        Scanner teclado = new Scanner(System.in);
         PrintStream saida = new PrintStream(cliente.getOutputStream());
         
         while (teclado.hasNextLine()) {
             saida.println(teclado.nextLine());
         }
         saida.close();
-        teclado.close();
         cliente.close();
+    }
+
+    public static personagem escolherPersonagem(){
+        
+        personagem [] p = {
+            new personagem("Arqueiro", 20, 20, 8, "mirar"),
+            new personagem("Bárbaro", 25, 25, 10, "furia"),
+            new personagem("Cavaleiro", 30, 30, 6, "defender"),
+            new personagem("Mago", 15, 15, 6, "mago"),
+        };
+
+        System.out.print("Digite sua opção de classe: ");
+        int opcao = teclado.nextInt();
+        
+        while (opcao != 1 || opcao != 2 || opcao != 3 || opcao != 4){
+            if(opcao == 1)
+                return p[0]; 
+            else if(opcao == 2)
+                return p[1]; 
+            else if(opcao == 3)
+                return p[2]; 
+            else if(opcao == 4)
+                return p[3];
+
+            System.out.print("Opção inválida, digite novamente: ");
+            opcao = teclado.nextInt();
+        }
+        
+        return null;
     }
 }
