de442a760180e2d0ba3c6aab3b720decc7b22370
pedro <ph.honorato28@gmail.com>
2022-09-16 00:01:05 -0300

    classe de personagem

--- Cliente.java
+++ Cliente.java
@@ -4,6 +4,60 @@ import java.io.PrintStream;
 import java.net.*;
 import java.util.Scanner;
 
+class Personagem {
+
+    //Características
+    private String nome;
+    private String estado;
+
+    //Atributos
+    private int f; //FOR
+    private int i; //INT
+    private int a; //AGI
+    private int m; //MIS
+    private int c; //CAR
+    private int p; //PRO
+    
+    //Scanner
+    private Scanner s = new Scanner(System.in);
+
+    public Personagem(){
+
+        System.out.println("\nDigite as características do seu personagem conforme forem solicitadas e tecle ENTER ao finalizar.\n");
+        
+        System.out.print("Nome: ");
+        nome = s.nextLine(); 
+
+        System.out.print("Estado Atual: ");
+        estado = s.nextLine(); 
+
+
+        System.out.println("\n\nDigite os atributos do seu personagem conforme forem solicitadas e tecle ENTER ao finalizar.\n");
+        
+        System.out.print("FOR: ");
+        f = s.nextInt();
+        
+        System.out.print("INT: ");
+        i = s.nextInt();
+        
+        System.out.print("AGI: ");
+        a = s.nextInt();
+        
+        System.out.print("MIS: ");
+        m = s.nextInt();
+        
+        System.out.print("CAR: ");
+        c = s.nextInt();
+        
+        System.out.print("PRO: ");
+        p = s.nextInt();
+        
+        System.out.println("\n\nCriação de personagem concluída!\n");
+    }
+
+}
+
+
 class Recebedor implements Runnable {
 
     private InputStream servidor;
@@ -31,7 +85,7 @@ public class Cliente {
 
     private static String host = "127.0.0.1";
     private static int porta = 12345;
-    //private static Personagem personagem; 
+    private static Personagem personagem; 
     //private static Acoes acoes;
 
     public static void main(String[] args) throws UnknownHostException, IOException {    
@@ -53,7 +107,7 @@ public class Cliente {
         PrintStream saida = new PrintStream(cliente.getOutputStream());
 
         //Cria um novo personagem e suas acoes
-        //personagem = new Personagem();
+        personagem = new Personagem();
         //acoes = new acoes(personagem);
 
         while (teclado.hasNextLine()) {
