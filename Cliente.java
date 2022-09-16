4fd97c7cae7eda4798bccbce25d507cd8010941e
pedro <ph.honorato28@gmail.com>
2022-09-15 23:45:31 -0300

    Nova refatoração

--- Cliente.java
+++ Cliente.java
@@ -4,36 +4,20 @@ import java.io.PrintStream;
 import java.net.*;
 import java.util.Scanner;
 
-class personagem {
-
-    public String classe;
-    public int maxVida;
-    public int atualVida;
-    public int dano;
-    public String acao;
-
-    public personagem(String classe, int maxVida, int atualVida, int dano, String acao){
-        this.classe = classe;
-        this.maxVida = maxVida;
-        this.atualVida = atualVida;
-        this.dano = dano;
-        this.acao = acao;
-    }
-
-}
-
 class Recebedor implements Runnable {
-    
+
     private InputStream servidor;
-    
+
     public Recebedor(InputStream servidor) {
         this.servidor = servidor;
     }
-    
+
     public void run() {
-        // recebe msgs do servidor e imprime na tela
-        Scanner s = new Scanner(this.servidor);
         
+        //Instancia um scanner para mensagens vindas do servidor
+        Scanner s = new Scanner(this.servidor);
+
+        //Exibe todas as mensagens vindas do servidor
         while (s.hasNextLine()) {
             System.out.println(s.nextLine());
         }
@@ -47,64 +31,37 @@ public class Cliente {
 
     private static String host = "127.0.0.1";
     private static int porta = 12345;
-    public static Scanner teclado = new Scanner(System.in);
-
-    public static personagem p;
-
-
-    public static void executa() throws UnknownHostException, IOException {
-        
-        Socket cliente = new Socket(host, porta);
-        System.out.println("Aguardando outros jogadores....");
-
-        // thread para receber mensagens do servidor
-        Recebedor r = new Recebedor(cliente.getInputStream());
-        new Thread(r).start();
-        
-        // lê msgs do teclado e manda pro servidor
-        PrintStream saida = new PrintStream(cliente.getOutputStream());
-        
-        while (teclado.hasNextLine()) {
-            saida.println(teclado.nextLine());
-        }
-        saida.close();
-        cliente.close();
-    }
-
-    public static personagem escolherPersonagem(){
-        
-        personagem [] p = {
-            new personagem("Arqueiro", 20, 20, 8, "mirar"),
-            new personagem("Bárbaro", 25, 25, 10, "furia"),
-            new personagem("Cavaleiro", 30, 30, 6, "defender"),
-            new personagem("Mago", 15, 15, 6, "mago"),
-        };
-
-        System.out.print("Digite sua opção de classe: ");
-        int opcao = teclado.nextInt();
-        
-        while (opcao != 1 || opcao != 2 || opcao != 3 || opcao != 4){
-            if(opcao == 1)
-                return p[0]; 
-            else if(opcao == 2)
-                return p[1]; 
-            else if(opcao == 3)
-                return p[2]; 
-            else if(opcao == 4)
-                return p[3];
-
-            System.out.print("Opção inválida, digite novamente: ");
-            opcao = teclado.nextInt();
-        }
-        
-        return null;
-    }
+    //private static Personagem personagem; 
+    //private static Acoes acoes;
 
     public static void main(String[] args) throws UnknownHostException, IOException {    
         
-        // p = escolherPersonagem();
-
-        // dispara cliente
         executa();
+    }
+
+    public static void executa() throws UnknownHostException, IOException {
+
+        //Cria um novo cliente
+        Socket cliente = new Socket(host, porta);
+
+        //Cria uma thread para receber mensagens do servidor
+        Recebedor r = new Recebedor(cliente.getInputStream());
+        new Thread(r).start();
+
+        //Lê msgs do teclado e manda pro servidor
+        Scanner teclado = new Scanner(System.in);
+        PrintStream saida = new PrintStream(cliente.getOutputStream());
+
+        //Cria um novo personagem e suas acoes
+        //personagem = new Personagem();
+        //acoes = new acoes(personagem);
+
+        while (teclado.hasNextLine()) {
+            saida.println(teclado.nextLine());
+        }
+        
+        saida.close();
+        teclado.close();
+        cliente.close();
     }
 }
--- Servidor.java
+++ Servidor.java
@@ -7,49 +7,8 @@ import java.util.List;
 import java.util.Scanner;
 
 
-class Historia {
-
-    public String escolherPersonagem(){
-        String [] linhas = {
-            "",
-            "",
-            "",
-            "",
-            "Existem quatro opções de classe:",
-            "",
-            "Arqueiro (1)\tBárbaro (2)\tCavaleiro (3)\tMago (4)",
-            "",
-            "Vida: 20\tVida: 25\tVida: 30\tVida: 15",
-            "Ataque: 1xD8\tAtaque: 1xD10\tAtaque: 1xD6\tAtaque: 1xD6",
-            "Ação: Mirar\tAção: Fúria\tAção: Defender\tAção: Curar",
-            "",
-            "",
-            "",
-        };
-
-        String texto = "";
-        for (String l: linhas){texto += l + '\n';}
-        return texto;
-    }
-
-    public void personagemEscolhido(String p){
-        String [] linhas = {
-            "",
-            "Você se conectou utilizando a classe: " + p,
-            "",
-        };
-
-        for (String l: linhas){System.out.println(l);}
-    }
-
-    public void inicio(){
-        
-    }
-}
-
-
 class TrataCliente implements Runnable {
-    
+
     private InputStream cliente;
     private Servidor servidor;
 
@@ -59,13 +18,15 @@ class TrataCliente implements Runnable {
     }
 
     public void run() {
-        // quando chegar uma msg, distribui pra todos
+        
+        //Instancia um scanner para o cliente
         Scanner s = new Scanner(this.cliente);
-        
+
+        //Distribui a mensagem pra todos os outros clientes
         while (s.hasNextLine()) {
-            servidor.distribuirMensagem(s.nextLine());
+            servidor.distribuiMensagem(s.nextLine());
         }
-        
+
         s.close();
     }
 }
@@ -73,61 +34,40 @@ class TrataCliente implements Runnable {
 
 
 public class Servidor {
-     
-    //Propriedades do Servidor
-    private int maxJogadores = 2;
-    private int numJogadores = 0;
-    private int porta = 12345;
-    private List<PrintStream> clientes = new ArrayList<PrintStream>();
-    
-    //História
-    public static Historia h = new Historia();
 
-    public void executa () throws IOException {
+    private int porta = 12345;
+    private List<PrintStream> clientes = new ArrayList<PrintStream>();;
+
+    public void executa() throws IOException {
 
         try (ServerSocket servidor = new ServerSocket(porta)) {
-            
             System.out.println("Porta " + porta + " aberta!");
-            boolean accept = true;
 
-            while (accept) {
-
-                //Aceita um cliente
+            while (true) {
+                //Aceito um cliente
                 Socket cliente = servidor.accept();
                 System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
 
                 //Adiciona saida do cliente à lista 
                 PrintStream ps = new PrintStream(cliente.getOutputStream());
-                clientes.add(ps);
+                this.clientes.add(ps);
 
-                //Cria tratador de cliente numa nova thread 
+                //Cria tratador de cliente em uma nova thread 
                 TrataCliente tc = new TrataCliente(cliente.getInputStream(), this);
                 new Thread(tc).start();
-
-                //soma no número de jogadores atuais
-                this.numJogadores++;
-
-                if(numJogadores == maxJogadores)
-                    accept = false;  
-                
             }
-
-            distribuirMensagem(h.escolherPersonagem());
-
         }
     }
 
-    public void distribuirMensagem(String msg) {
-        // envia msg para todo mundo
+    public void distribuiMensagem(String msg) {
+        //Envia a memsagem para todos os clientes
         for (PrintStream cliente : this.clientes) {
             cliente.println(msg);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        
-        // inicia o servidor
+        //Inicia o servidor
         new Servidor().executa();
-    
     }
 }
